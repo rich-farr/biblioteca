c984a6bfaff6f16c8614bd8dcea8a2b8f41e7412
rich-farr <scarletmanuka@gmail.com>
2016-07-20 17:57:19 +1200

    Add more styling

--- components/book.js
+++ components/book.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router'
 const Book = (props) => {
   let linkTo = "/books/" + props.id
   return (
-    <div>
+    <div className="book">
+      <Link to={linkTo}><img src={props.image} /></Link>
       <h1>{props.title} ({props.year})</h1>
       <h3>{props.author}</h3>
-      <Link to={linkTo}><img src={props.image} /></Link>
     </div>
     )
 }
