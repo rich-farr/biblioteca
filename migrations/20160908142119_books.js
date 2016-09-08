be3f5e7e5df8a5545b5aa572e348426e4ea8259f
rich-farr <scarletmanuka@gmail.com>
2016-09-08 14:28:44 +1200

    Delete extra migration files, add add and drop table functions

--- migrations/20160908142119_books.js
+++ migrations/20160908142119_books.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTableIfNotExists('books', table => {
+
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('books')
 };
--- migrations/20160908142131_authors.js
+++ migrations/20160908142131_authors.js
@@ -1,8 +1,10 @@
 
 exports.up = function(knex, Promise) {
-  
+  return knex.schema.createTableIfNotExists('authors', table => {
+
+  })
 };
 
 exports.down = function(knex, Promise) {
-  
+  return knex.schema.dropTableIfExists('authors')
 };
