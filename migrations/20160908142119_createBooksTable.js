563b4baee7995ad0462ff7921dce807a05fe9302
rich-farr <scarletmanuka@gmail.com>
2016-09-08 14:45:26 +1200

    Add columns to both migration files

--- migrations/20160908142119_createBooksTable.js
+++ migrations/20160908142119_createBooksTable.js
@@ -1,7 +1,16 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('books', table => {
-
+    table.increments('id').primary
+    table.string('title').notNullable()
+    table.integer('year').notNullable()
+    table.string('isbn')
+    table.string('my_description')
+    table.string('google_description')
+    table.boolean('have_read').notNullable()
+    table.boolean('lent_it').notNullable()
+    table.boolean('wishlist').notNullable()
+    table.integer('author_id').notNullable()
   })
 };
 
--- migrations/20160908142131_createAuthorsTable.js
+++ migrations/20160908142131_createAuthorsTable.js
@@ -1,7 +1,9 @@
 
 exports.up = function(knex, Promise) {
   return knex.schema.createTableIfNotExists('authors', table => {
-
+    table.increments('id').primary
+		table.string('first_name')
+		table.string('last_name')
   })
 };
 
