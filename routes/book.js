10e356d8df99481f409f3e28cc8dcbd300559a0c
rich-farr <scarletmanuka@gmail.com>
2016-09-12 15:36:03 +1200

    Return postgres data as json from api routes

--- routes/book.js
+++ routes/book.js
@@ -1,8 +1,23 @@
 const express = require('express')
 const router = express.Router()
+const Knex = require('knex')
+const knexConfig = require('../knexfile')
+
+const knex = Knex(knexConfig[process.env.NODE_ENV || 'development'])
 
 router.get('/', (req, res, next) => {
-  console.log("AAA", res)
+  return knex.select().from('books')
+  .then(books => {
+    if (books) {
+      res.json(books)
+    } else {
+      res.sendStatus(404)
+    }
+  })
+  .catch(err => {
+    console.error(err)
+    res.json(err)
+  })
 })
 
 module.exports = router
