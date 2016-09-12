287271e0579f10434fe6d5fca4473df86870ef19
rich-farr <scarletmanuka@gmail.com>
2016-09-12 13:21:02 +1200

    Set up server and create server route folder and files

--- server.js
+++ server.js
@@ -5,7 +5,21 @@ import path from 'path'
 const app = express()
 const server = http.createServer(app)
 
+// Middleware
+
+const bodyParser = require('body-parser')
+const bookRoutes = require('/routes/book')
+const authorRoutes = require('/routes/author')
+
+// Hooking up Express middleware
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, '/public')))
+
+
+// app.use('/api/', sessionRoutes)
+app.use('/api/books', userRoutes)
+app.use('/api/authors', gameRoutes)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
@@ -14,3 +28,5 @@ app.get('*', (req, res) => {
 server.listen(3000, () => {
   console.log('Super l33t server on port 3000 is running')
 })
+
+module.exports = app
