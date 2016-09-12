c47f267b66a2d0dd710888911468a4ae5eee08dd
rich-farr <scarletmanuka@gmail.com>
2016-09-12 14:09:51 +1200

    Create www file, install nodemon, add db line to package.json, get express server half working

--- routes/book.js
+++ routes/book.js
@@ -1,6 +1,6 @@
-const express = require('express')
-const router = express.Router()
-
-router.get('/', (req, res, next) => {
-  console.log("AAA", res)
-})
+// const express = require('express')
+// const router = express.Router()
+//
+// router.get('/', (req, res, next) => {
+//   console.log("AAA", res)
+// })
--- server.js
+++ server.js
@@ -1,15 +1,13 @@
-import express from 'express'
-import http from 'http'
-import path from 'path'
+const express = require('express')
+const path = require('path')
 
 const app = express()
-const server = http.createServer(app)
 
 // Middleware
 
 const bodyParser = require('body-parser')
-const bookRoutes = require('/routes/book')
-const authorRoutes = require('/routes/author')
+const bookRoutes = require('./routes/book')
+const authorRoutes = require('./routes/author')
 
 // Hooking up Express middleware
 app.use(bodyParser.json())
@@ -18,15 +16,11 @@ app.use(express.static(path.join(__dirname, '/public')))
 
 
 // app.use('/api/', sessionRoutes)
-app.use('/api/books', userRoutes)
-app.use('/api/authors', gameRoutes)
+// app.use('/api/books', bookRoutes)
+// app.use('/api/authors', authorRoutes)
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/index.html'))
-})
-
-server.listen(3000, () => {
-  console.log('Super l33t server on port 3000 is running')
 })
 
 module.exports = app
